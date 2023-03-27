dc38558818796d27a07269cc5caa8d4fadea8b42
jessicasilva <109664683+jessicaa23@users.noreply.github.com>
2023-03-27 12:30:29 -0300

    Add files via upload

---
+++ arrays/script.js
@@ -0,0 +1,11 @@
+const input = document.getElementById('input')
+const adicionar = document.getElementById('adicionar')
+const ordenar = document.getElementById('ordenar')
+
+ adicionar.addEventListener('click', function(){
+    console.log(array.length);
+    for(let person of people){
+        console.log(person)
+    }
+ })
+ 
